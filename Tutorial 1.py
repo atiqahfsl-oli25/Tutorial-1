2cc0b1db1a6d8546bdb871e4176d2401a5674195
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-10-19 15:02:52 +0800

    Tutorial 1.py

--- Tutorial 1.py
+++ Tutorial 1.py
@@ -13,7 +13,6 @@ st.title("🎓 Arts Faculty Gender Distribution")
 url = "https://raw.githubusercontent.com/atiqahfsl-oli25/Tutorial-1/refs/heads/main/arts_faculty_data.csv"
 
 col1, col2, col3, col4 = st.columns(4)
-    
     col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
     col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
     col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
